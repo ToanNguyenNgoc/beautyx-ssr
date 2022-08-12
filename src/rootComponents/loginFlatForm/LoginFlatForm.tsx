cc7883e612ec258d5f461f02912cb27f21fd6671
Nguyễn Quang Khải <khai@myspa.vn>
2022-08-12 14:39:03 +0700

    [nothing]

--- src/rootComponents/loginFlatForm/LoginFlatForm.tsx
+++ src/rootComponents/loginFlatForm/LoginFlatForm.tsx
@@ -9,7 +9,7 @@ import {
 } from "../../redux/loginFlatForm/loginFlatFrom";
 import { pickBy, identity } from "lodash";
 import MOMO from '../../api/_momoImport';
-import { IUserConsentsData } from "../../api/momoApi";
+import momoApi, { IUserConsentsData } from "../../api/momoApi";
 // import momoAuthApi from '../../api/_momoAuthApi';
 // import { AnyAaaaRecord } from 'dns';
 
@@ -20,7 +20,7 @@ function LoginFlatForm(props: any) {
     const onLoginFlatFormMomo = async () => {
         try {
             alert('onLoginFlatFormMomo')
-            
+            momoApi.initApp();
             MOMO.showLoading([""]);
             MOMO.getUserConsents({
                 "permissions": [
