5bee04128d730614cf6a5a498992c1eaa0640f9f
Nguyễn Quang Khải <khai@myspa.vn>
2022-08-17 10:05:14 +0700

    [UPDATE] fix onerror img

--- src/utils/errorImg.ts
+++ src/utils/errorImg.ts
@@ -1,6 +1,7 @@
 import img from "../constants/img";
 
-export default function onErrorImg(e: any) {
+export default function onErrorImg(e: any,isDisable?: boolean) {
+    if (isDisable) { e.target.style.display = 'none';return}
     e.target.src = img.imgDefault;
     e.target.style.objectFit = "contain";
     //e.target.style.transform = "scale(0.5)";
