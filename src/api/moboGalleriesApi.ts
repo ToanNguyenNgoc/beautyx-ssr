c8908bdc9f1917d0e019c8ce077ccf2d51c568e0
nguyentoan06 <toan@myspa.vn>
2022-09-27 11:20:26 +0700

    [UPDATE[ fix moba galleries

--- src/api/moboGalleriesApi.ts
+++ src/api/moboGalleriesApi.ts
@@ -5,7 +5,6 @@ class MobaGalleries {
         const url = `/organizations/${org_id}/moba_galleries`;
         const params = {
             page: 1,
-            limit: 15,
             include: "images|videos",
         };
         return axiosClient.get(url, { params });
