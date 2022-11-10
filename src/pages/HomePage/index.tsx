45e2213d6049f14fb8ca04c6ab6531db67723d8d
Đỗ Cao Long <long@myspa.vn>
2022-11-10 16:24:07 +0700

    [UPDATE] create section province & section partners page home

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -15,22 +15,22 @@ import Head from "features/Head";
 import { LoadHomeBanner } from "components/LoadingSketion/LoadHome";
 import HomeDiscount from "features/HomeDiscounts";
 import Footer from "features/Footer";
-import { Bottom, OpenApp } from 'components/Layout'
+import { Bottom, OpenApp } from "components/Layout";
 import { useDeviceMobile } from "utils";
 import HomeBanner2 from "./HomeBanner2";
 import HomeCate2 from "./HomeCate2";
 import HomeTags2 from "./HomeTags2";
-import './home-se.css'
+import "./home-se.css";
 import HomeTopic from "./HomeTopic";
+import HomeProvince2 from "./HomeProvince2";
+import HomePartners from "./HomePartners";
 export default function HomePage() {
-    const IS_MB = useDeviceMobile()
+    const IS_MB = useDeviceMobile();
     const banner_status = useSelector((state: any) => state.HOME.status);
 
     useEffect(() => {
         tracking.HOME_LOAD();
-    }, [])
-
-
+    }, []);
 
     return (
         <div className="homepage">
@@ -39,24 +39,24 @@ export default function HomePage() {
             <div className="home_container_par">
                 <Container>
                     {!IS_MB && <HomeCate2 />}
-                    {
-                        banner_status !== STATUS.SUCCESS ?
-                            <LoadHomeBanner />
-                            :
-                            <>
-                                <HomeBanner2 />
-                                <HomeTags2 />
-                            </>
-                    }
+                    {banner_status !== STATUS.SUCCESS ? (
+                        <LoadHomeBanner />
+                    ) : (
+                        <>
+                            <HomeBanner2 />
+                            <HomeTags2 />
+                        </>
+                    )}
                 </Container>
             </div>
             <HomeDiscount />
             <Container>
                 <HomeOrgDistance />
-                <HomeTopic/>
+                <HomeTopic />
                 <HomeRecomment />
                 <HomeWatched />
-                <HomeProvince />
+                <HomeProvince2 />
+                <HomePartners />
             </Container>
             <Footer />
             <Bottom />
