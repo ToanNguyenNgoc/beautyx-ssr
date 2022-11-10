dab9708111d9641f87c577283fa89b3cb77a262d
Đỗ Cao Long <long@myspa.vn>
2022-11-10 10:15:19 +0700

    [UPDATE] merge Dev_remake

--- src/utils/customChat.ts
+++ src/utils/customChat.ts
@@ -2,7 +2,7 @@ import { FLAT_FORM_TYPE } from "../rootComponents/flatForm";
 import { callApiFromTiki } from "../rootComponents/tiki/doPostMessageTiki";
 import { openUrlIn_Mb } from "../rootComponents/mb/doPostMessageMBbank";
 import openUrlIn_Tiki from "../rootComponents/tiki/test";
-import { MOMO } from "../api/_momoImport";
+// import { MOMO } from "../api/_momoImport";
 export const handleSubiz = () => {
     let $: any = window;
     let su_widget = document.querySelector(
@@ -34,8 +34,7 @@ export const handleChat = () => {
             // window.open(deepLinkMessenger, "_blank");
             break;
         case FLAT_FORM_TYPE.MOMO:
-            
-            MOMO.openURL(deepLinkMessenger);
+            // MOMO.openURL(deepLinkMessenger);
             // alert(deepLinkMessenger)
             break;
         case FLAT_FORM_TYPE.MB:
@@ -65,18 +64,20 @@ export const handleCallingPhone = () => {
             window.open(phoneNumber, "_blank");
             break;
         case FLAT_FORM_TYPE.MOMO:
-            MOMO.openURL(phoneNumber);
+            // MOMO.openURL(phoneNumber);
             // alert(phoneNumber)
             break;
         case FLAT_FORM_TYPE.MB:
             // openUrlIn_Mb('messenger',phoneNumber)
-            const $:any = window;
-            $['ReactNativeWebView'].postMessage(JSON.stringify({
-                type: 'TEL',
-                data: {
-                    tel: phoneNumber
-                }
-            }));
+            const $: any = window;
+            $["ReactNativeWebView"].postMessage(
+                JSON.stringify({
+                    type: "TEL",
+                    data: {
+                        tel: phoneNumber,
+                    },
+                })
+            );
             break;
         default:
             window.open(phoneNumber, "_blank");
