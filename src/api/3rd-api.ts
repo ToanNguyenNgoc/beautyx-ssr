d7ea7f23677a7b37ab148d59076f90f264d51838
nguyentoan06 <toan@myspa.vn>
2023-05-16 14:18:15 +0700

    [REBUILD] 16/05

--- src/api/3rd-api.ts
+++ src/api/3rd-api.ts
@@ -1,7 +1,6 @@
 const keyMapBox = process.env.REACT_APP_MAPBOX_TOKEN
 
 const API_3RD = {
-    // API_NODE: "https://f0f1-42-117-89-185.ap.ngrok.io/v1"
     API_NODE: "https://api-node-myspa.vercel.app/v1",
     API_MAP_BOX: (lat: string | number, lng: string | number) => {
         return `https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json?access_token=${keyMapBox}&language=vi&country=vn`
