38408e22c845cf9d26b1a614d404ee549270bd81
nguyentoan06 <toan@myspa.vn>
2023-05-30 10:33:25 +0700

    ...

--- src/index.tsx
+++ src/index.tsx
@@ -24,25 +24,9 @@ import { AUTH_HEADER } from "api/authHeader";
 //     tracesSampleRate: 1.0,
 // });
 
-ReactDOM.render(
-    <React.StrictMode>
-        <SWRConfig
-            value={{
-                fetcher: (url) => axiosClient.get(url, AUTH_HEADER()),
-                shouldRetryOnError: false
-            }}
-        >
-            <Provider store={store}>
-                <Suspense fallback={<PlashScreen />}>
-                    <App />
-                </Suspense>
-            </Provider>
-        </SWRConfig>
-    </React.StrictMode>,
-    document.getElementById("app")
-);
+ReactDOM.hydrate(<App />, document.getElementById('app'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
